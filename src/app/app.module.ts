6198a093356e83bcfc0f674b1daad7ff4a98dee6
mimi <mimibhaj@gmail.com>
2021-09-27 16:19:12 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashbordComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dashbord/dashbord.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashbord',
+  templateUrl: './dashbord.component.html',
+  styleUrls: ['./dashbord.component.css']
+})
+export class DashbordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  function(){
+    let btn:any = document.querySelector("#btn");
+    let sidebar:any = document.querySelector(".sidebar");
+    let searchBtn = document.querySelector(".bx-search");
+
+
+    sidebar.classList.toggle("active");
+    if(btn.classList.contains("bx-menu")) {
+        btn.classList.replace("bx-menu", "bx-menu-alt-right");
+    } else {
+        btn.classList.replace("bx-menu-alt-right", "bx-menu");
+    }
+  }
+
+
+}
